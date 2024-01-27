aa673bdcdc26218d0ceb1a52b5018de6731d58bc
Elshad Sadigov <sv.elshad@gmail.com>
2024-01-27 23:36:25 +0400

    home page codeing

--- home.js
+++ home.js
@@ -1,92 +1,111 @@
+// Search location
+let locationText = document.querySelector('#locationText')
+let optionsLocation = document.querySelector('.optionsLocation')
+
+optionsLocation.onclick = function(){
+    locationText.innerHTML = this.textContent
+    locationText.parentElement.parentElement.classList.toggle("activeDiv")
+}
+// Search location
+
+// Serach destination
+let destinationText = document.querySelector('#destinationText')
+let optionsDestination = document.querySelectorAll('.optionsDestination')
+
+for(optionDestination of optionsDestination){
+    optionDestination.onclick = function(){
+        destinationText.innerHTML = this.textContent
+        destinationText.parentElement.parentElement.classList.toggle('activeDiv')
+    }
+}
+// Serach destination
+
+
 // Home Ratings
-let selectRatings = document.querySelector('#selectRatings')
 let ratingsText = document.querySelector('#ratingsText')
 let optionsRatings = document.querySelectorAll('.optionsRatings')
-let listRatings = document.querySelector('#listRatings')
 
 for (optionRating of optionsRatings) {
     optionRating.onclick = function () {
         ratingsText.innerHTML = this.textContent
-        listRatings.classList.toggle("show")
+        ratingsText.parentElement.parentElement.classList.toggle("activeDiv")
     }
 }
-selectRatings.onclick = function () {
-    listRatings.classList.toggle("show")
-}
+
 // Home Ratings
 
 // Home Nutrition
-let selectNutrition = document.querySelector('#selectNutrition')
 let nutritionText = document.querySelector('#nutritionText')
 let optionsNutritions = document.querySelectorAll('.optionsNutritions')
-let listNutritions = document.querySelector('#listNutritions')
 
 for (optionNutrition of optionsNutritions) {
     optionNutrition.onclick = function () {
         nutritionText.innerHTML = this.textContent
-        listNutritions.classList.toggle("show")
+        nutritionText.parentElement.parentElement.classList.toggle("activeDiv")
     }
 }
-selectNutrition.onclick = function () {
-    listNutritions.classList.toggle('show')
-}
+
 // Home Nutrition
 
 // Home Nights
-let selectNight = document.querySelector('#selectNight')
 let nightText = document.querySelector('#nightText')
-let listNight = document.querySelector('#listNight')
 let optionsNights = document.querySelectorAll('.optionsNights')
 
 for (optionNight of optionsNights) {
     optionNight.onclick = function () {
         nightText.innerHTML = this.textContent
-        listNight.classList.toggle('show')
+        nightText.parentElement.parentElement.classList.toggle('activeDiv')
     }
-}
-selectNight.onclick = function () {
-    listNight.classList.toggle('show')
 }
 // Home Nights
 
+
+const clickDiv = document.querySelectorAll(".clickDiv")
+
+clickDiv.forEach(e => {
+    e.addEventListener('click', ()=>{
+        e.parentElement.classList.toggle("activeDiv")
+    })
+})
+
 // Home Guest
-let selectGuest = document.querySelector('#selectGuest')
-let guestText = document.querySelector('#guestText')
-let listGuest = document.querySelector('#listGuest')
-let adultCount = document.querySelector('#adultCount')
-let adultIncrease = document.querySelector('#adultIncrease')
-let adultDecrease = document.querySelector('#adultDecrease') 
-let childIncrease = document.querySelector('#childIncrease')
-let childDecrease = document.querySelector('#childDecrease')
-let childCount = document.querySelector('#childCount')
+// let selectGuest = document.querySelector('#selectGuest')
+// let guestText = document.querySelector('#guestText')
+// let listGuest = document.querySelector('#listGuest')
+// let adultCount = document.querySelector('#adultCount')
+// let adultIncrease = document.querySelector('#adultIncrease')
+// let adultDecrease = document.querySelector('#adultDecrease') 
+// let childIncrease = document.querySelector('#childIncrease')
+// let childDecrease = document.querySelector('#childDecrease')
+// let childCount = document.querySelector('#childCount')
 
 
 
-adultDecrease.onclick = function () {
-    if (adultCount.innerHTML > 0) {
-        adultCount.innerHTML = Number(adultCount.innerHTML) - 1
-    }
-}
-adultIncrease.onclick = function () {
-    adultCount.innerHTML = Number(adultCount.innerHTML) + 1
-}
+// adultDecrease.onclick = function () {
+//     if (adultCount.innerHTML > 0) {
+//         adultCount.innerHTML = Number(adultCount.innerHTML) - 1
+//     }
+// }
+// adultIncrease.onclick = function () {
+//     adultCount.innerHTML = Number(adultCount.innerHTML) + 1
+// }
 
-childDecrease.onclick = function () {
-    if (childCount.innerHTML > 0) {
-        childCount.innerHTML = Number(childCount.innerHTML) - 1
-    }
-}
-childIncrease.onclick = function () {
-    childCount.innerHTML = Number(childCount.innerHTML) + 1
-}
+// childDecrease.onclick = function () {
+//     if (childCount.innerHTML > 0) {
+//         childCount.innerHTML = Number(childCount.innerHTML) - 1
+//     }
+// }
+// childIncrease.onclick = function () {
+//     childCount.innerHTML = Number(childCount.innerHTML) + 1
+// }
 
-selectGuest.onclick = function () {
-    listGuest.classList.toggle("show")
-    if (listGuest.classList.contains('show')) {
-        guestText.innerHTML = guestText.innerHTML
-    } else if (adultCount.innerHTML > 0 || childCount.innerHTML > 0) {
-        guestText.innerHTML = `${adultCount.innerHTML} adult, ${childCount.innerHTML} child`
+// selectGuest.onclick = function () {
+//     listGuest.classList.toggle("show")
+//     if (listGuest.classList.contains('show')) {
+//         guestText.innerHTML = guestText.innerHTML
+//     } else if (adultCount.innerHTML > 0 || childCount.innerHTML > 0) {
+//         guestText.innerHTML = `${adultCount.innerHTML} adult, ${childCount.innerHTML} child`
 
-    }
-}
+//     }
+// }
 // Home Guest
